74782a375634e9314e3bf15e734999f3e3819a35
Peter Green <fishlegs10@gmail.com>
2020-09-02 21:33:24 -0400

    933

---
+++ src/app/project-module/project-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-module',
+  templateUrl: './project-module.component.html',
+  styleUrls: ['./project-module.component.css']
+})
+export class ProjectModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
