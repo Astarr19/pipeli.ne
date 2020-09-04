4ccb7fc63631a6b2aabca904ea904fd4393c3186
Peter Green <fishlegs10@gmail.com>
2020-09-03 20:40:43 -0400

    840

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+import { FilterComponent } from './filter/filter.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
